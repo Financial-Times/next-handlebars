3533915b4cf5c286b5599be48cf62963adae9833
andygnewman <andrew.newman@ft.com>
2016-10-19 16:10:11 +0100

    get referencing right

--- src/helpers/presenter.js
+++ src/helpers/presenter.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const Handlebars = require('handlebars');
 
 module.exports = (presenterPath, context, options) => {
-	const presenterName = presenterPath.split('/').pop.replace(/-([a-z])/g, g => {g[1].toUpperCase()});
-	const Presenter = require(`${process.cwd}${presenterPath}`);
+	const presenterName = path.basename(presenterPath).replace(/-([a-z])/g, g => g[1].toUpperCase());
+	let Presenter;
+	if (presenterPath.startsWith('.')) {
+		Presenter = require(path.join(process.cwd(), presenterPath));
+	} else {
+		Presenter = require(presenterPath);
+	}
 	if (options.data) {
 		const data = Handlebars.createFrame(options.data);
 		data[presenterName] = new Presenter(context);
