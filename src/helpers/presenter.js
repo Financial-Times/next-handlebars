85d62d325feee3827565c357697ba412b91ed780
andygnewman <andrew.newman@ft.com>
2016-10-19 10:59:47 +0100

    linting

--- src/helpers/presenter.js
+++ src/helpers/presenter.js
@@ -1,6 +1,6 @@
 module.exports = (presenterPath, context, options) => {
-  const presenterName = presenterPath.split('/').pop.replace(/-([a-z])/g, g => {g[1].toUpperCase()});
-  const Presenter = require(`${process.cwd}${presenterPath}`);
+	const presenterName = presenterPath.split('/').pop.replace(/-([a-z])/g, g => {g[1].toUpperCase()});
+	const Presenter = require(`${process.cwd}${presenterPath}`);
 	if (options.data) {
 		const data = Handlebars.createFrame(options.data);
 		data[presenterName] = new Presenter(context);
