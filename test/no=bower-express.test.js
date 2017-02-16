4dd2fbc9cd352a87dde68ed0e683c1a191039f48
andygout <andy.gout@ft.com>
2017-02-16 13:02:39 +0000

    Fix: Linting errors

--- test/no=bower-express.test.js
+++ test/no=bower-express.test.js
@@ -1,15 +1,15 @@
 /*global it, describe, beforeEach, before*/
-"use strict";
+'use strict';
 
-var request = require('supertest');
-var app = require('./fixtures/no-bower-app/main');
+const request = require('supertest');
+const app = require('./fixtures/no-bower-app/main');
 
-describe('express handlebars setup', function() {
-	before(function() {
+describe('express handlebars setup', function () {
+	before(function () {
 		return app.promise;
 	});
 
-	it('should do templating', function(done) {
+	it('should do templating', function (done) {
 		request(app)
 			.get('/templated')
 			.expect(200, /FT/, done);
