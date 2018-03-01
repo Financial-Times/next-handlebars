cf7747f118777f6aa94a5007d0411f254f5333a7
Ben Fletcher <ben.fletcher@ft.com>
2018-03-01 13:34:18 +0000

    helpful error

--- src/helpers/inline.js
+++ src/helpers/inline.js
@@ -4,6 +4,9 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = function (opts) {
+	if (!opts || !opts.hash || !opts.hash.file) {
+		throw new Error('file option is mandatory');
+	}
 	const file = opts.hash.file;
-	return fs.readFileSync(path.join(process.cwd(), 'bower_components', opts.hash.file), 'utf-8');
+	return fs.readFileSync(path.join(process.cwd(), 'bower_components', file), 'utf-8');
 };
