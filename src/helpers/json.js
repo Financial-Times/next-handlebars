47452a9e6d2eacd1119d349b4161737565a7596a
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2018-04-26 12:57:44 +0100

    Don't allow JSON helper to output root context

--- src/helpers/json.js
+++ src/helpers/json.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function (obj) {
+	if (process.env.NODE_ENV !== 'development' && obj.hasOwnProperty('_locals') {
+		throw Error('DO NOT DO THIS');
+	}
+
 	return JSON.stringify(obj);
 };
